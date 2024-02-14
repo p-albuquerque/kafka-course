e9dc1cbcb51b0ab41e3dd4293c70896bb03d5d75
Paulo Albuquerque <paulo.albuquerque@liferay.com>
2024-02-14 20:21:51 -0300

    L-30 Template method: apply KafkaService for any existing KafkaConsume Service isolating their logic. Each Service only need to take care of it own behavior

--- ecommerce/src/main/java/br/com/alura/ecommerce/EmailService.java
+++ ecommerce/src/main/java/br/com/alura/ecommerce/EmailService.java
@@ -1,53 +1,26 @@
 package br.com.alura.ecommerce;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.StringDeserializer;
 
-import java.time.Duration;
-import java.util.Collections;
-import java.util.Properties;
+public class EmailService extends KafkaService {
 
-public class EmailService {
-
-    public static void main(String[] args) {
-        KafkaConsumer consumer = new KafkaConsumer<String, String>(properties());
-        consumer.subscribe(Collections.singletonList("ECOMMERCE_SEND_EMAIL"));
-
-        while(true) {
-            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
-
-            if (!records.isEmpty()) {
-                System.out.println("Encontrei " + records.count() + " registros");
-
-                for (ConsumerRecord record : records) {
-                    System.out.println("#####################################################");
-                    System.out.println("Enviando Email!!!");
-                    System.out.println("Key :: " + record.key());
-                    System.out.println("Value :: " + record.value());
-                    System.out.println("Partition :: " + record.partition());
-                    System.out.println("offset :: " + record.offset());
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println("Email Enviado");
-                }
-            }
-        }
+    public EmailService(String topic) {
+        super(EmailService.class.getSimpleName(), topic);
     }
 
-    private static Properties properties() {
-        Properties properties = new Properties();
-
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, EmailService.class.getSimpleName());
-
-        return properties;
+    @Override
+    protected void consume(ConsumerRecord<String, String> record) {
+        System.out.println("#####################################################");
+        System.out.println("Enviando Email!!!");
+        System.out.println("Key :: " + record.key());
+        System.out.println("Value :: " + record.value());
+        System.out.println("Partition :: " + record.partition());
+        System.out.println("offset :: " + record.offset());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Email Enviado");
     }
 }
--- ecommerce/src/main/java/br/com/alura/ecommerce/LogService.java
+++ ecommerce/src/main/java/br/com/alura/ecommerce/LogService.java
@@ -1,47 +1,20 @@
 package br.com.alura.ecommerce;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.StringDeserializer;
 
-import java.time.Duration;
-import java.util.Properties;
-import java.util.regex.Pattern;
+public class LogService extends KafkaService {
 
-public class LogService {
-
-    public static void main(String[] args) {
-        KafkaConsumer consumer = new KafkaConsumer<String, String>(properties());
-        consumer.subscribe(Pattern.compile("ECOMMERCE.*"));
-
-        while(true) {
-            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
-
-            if (!records.isEmpty()) {
-                System.out.println("Encontrei " + records.count() + " registros");
-
-                for (ConsumerRecord record : records) {
-                    System.out.println("------------------------------------------");
-                    System.out.println("LOG: " + record.topic());
-                    System.out.println("Key :: " + record.key());
-                    System.out.println("Value :: " + record.value());
-                    System.out.println("Partition :: " + record.partition());
-                    System.out.println("offset :: " + record.offset());
-                }
-            }
-        }
+    public LogService(String topic) {
+        super(LogService.class.getSimpleName(), topic);
     }
 
-    private static Properties properties() {
-        Properties properties = new Properties();
-
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, LogService.class.getSimpleName());
-
-        return properties;
+    @Override
+    protected void consume(ConsumerRecord<String, String> record) {
+        System.out.println("------------------------------------------");
+        System.out.println("LOG: " + record.topic());
+        System.out.println("Key :: " + record.key());
+        System.out.println("Value :: " + record.value());
+        System.out.println("Partition :: " + record.partition());
+        System.out.println("offset :: " + record.offset());
     }
 }
