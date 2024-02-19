5b780b9d3d837dc3758b468eea31dbd07195fef5
Paulo Albuquerque <paulo.albuquerque@liferay.com>
2024-02-18 21:58:00 -0300

    L-41 Apply Dispatcher

--- ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -1,29 +1,12 @@
 package br.com.alura.ecommerce;
 
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
-
-import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        // Feedback do envio das mensagens
-        Callback callback = (data, ex) -> {
-            if (ex != null) {
-                ex.printStackTrace();
-                return;
-            }
-            System.out.println("sucesso enviando " + data.topic() + ":::partition " + data.partition() + "/ offset " + data.offset());
-        };
-
-        // Preparando produtor e usando-o para enviar as mensagens dos seus respectivos tópicos
-        KafkaProducer producer = new KafkaProducer<String, String>(properties());
+        KafkaDispatcher kafkaDispatcher = new KafkaDispatcher();
 
         // Enviar 100 mensagens para _NEW_ORDER randomizando as partições que receberão
         for(int i = 0; i < 100; i++) {
@@ -31,25 +14,12 @@ public class NewOrderMain {
             String key = UUID.randomUUID().toString();
             String value = "item#" + i;
 
-            ProducerRecord newOrderRecord = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);
-            producer.send(newOrderRecord, callback).get();
+            kafkaDispatcher.send(key, value,"ECOMMERCE_SEND_EMAIL");
         }
 
         // Preparando mensagem para tópico _SEND_EMAIL
         String email = "Seu pedido foi submetido, iniciando análise, aguardando confirmação!";
 
-        ProducerRecord emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", "Email", email);
-
-        producer.send(emailRecord, callback).get();
-    }
-
-    private static Properties properties() {
-        Properties properties = new Properties();
-
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-
-        return properties;
+        kafkaDispatcher.send("Novo Email", email,"ECOMMERCE_NEW_ORDER");
     }
 }
